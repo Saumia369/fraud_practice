1a6aedd24c33383ebf6dcdeb3466c0a38d322697
saumia369 <saumiariavarghese@ce.ajce.in>
2022-03-07 00:58:54 +0530

    Added 3 new ml models

--- Model.py
+++ Model.py
@@ -1,2 +1,11 @@
 
 print("Adding all ML related code here")
+
+def model1():
+    print("Adding code for ml model1")
+
+def model2():
+    print("Adding code for ml model2")
+
+def model3():
+    print("Adding code for ml model3")
